6fc9fbb3a7f9c3d64ca3b5b29d9db61a84a70b4d
omargawdat <omargawdaat@gmail.com>
2023-10-11 22:32:24 +0300

    Management: Fix loading Fixtures command

--- apps/core/management/commands/load_all_fixtures.py
+++ apps/core/management/commands/load_all_fixtures.py
@@ -1,24 +1,33 @@
 import os
+import subprocess
 
-from django.conf import settings
-from django.core.management import call_command
 from django.core.management.base import BaseCommand
 
 
 class Command(BaseCommand):
-    help = "Load all fixture files"
+    help = 'Load all fixtures for all apps in the apps directory.'
 
-    def handle(self, *args, **options):
-        apps = settings.INSTALLED_APPS
+    def handle(self, *args, **kwargs):
+        # Step 1: Open the apps folder
+        apps_folder_path = 'apps'
 
-        for app in apps:
-            fixture_dir = os.path.join(settings.BASE_DIR, app, 'fixtures')
+        # Check if apps folder exists
+        if not os.path.exists(apps_folder_path):
+            self.stdout.write(self.style.ERROR('The "apps" folder does not exist.'))
+            return
 
-            if os.path.exists(fixture_dir):
-                for filename in os.listdir(fixture_dir):
-                    if filename.endswith('.json'):
-                        fixture_file = os.path.join(fixture_dir, filename)
-                        self.stdout.write(self.style.SUCCESS(f'Loading fixture {fixture_file}'))
-                        call_command('loaddata', fixture_file)
+        for app_name in os.listdir(apps_folder_path):
+            fixture_folder_path = os.path.join(apps_folder_path, app_name, 'fixtures')
 
-        self.stdout.write(self.style.SUCCESS('Successfully loaded all fixtures'))
+            # Check if the fixtures folder exists in the app directory
+            if os.path.exists(fixture_folder_path):
+                for fixture_file in os.listdir(fixture_folder_path):
+                    if fixture_file.endswith('.json'):
+                        fixture_file_path = os.path.join(fixture_folder_path, fixture_file)
+                        command = f"python manage.py loaddata {fixture_file_path}"
+
+                        try:
+                            subprocess.run(command, shell=True, check=True, capture_output=True, text=True)
+                            self.stdout.write(self.style.SUCCESS(f'Successfully loaded {fixture_file}'))
+                        except subprocess.CalledProcessError as e:
+                            self.stdout.write(self.style.ERROR(f'Failed to load {fixture_file}: {e.stderr}'))
