a2a071820ab8d69e486b11d31efd1fea29329d87
Nadeen-Badr <108485450+Nadeen-Badr@users.noreply.github.com>
2023-10-29 12:42:53 +0200

    Update urls.py

--- apps/depression_test/urls.py
+++ apps/depression_test/urls.py
@@ -6,5 +6,5 @@ from .views import TestQuestionAPIView
 urlpatterns = [
     path('testu/', views.TestAPIView.as_view(), name='test-api-class-view'),
     path('questions/', TestQuestionAPIView.as_view(), name='test_questions_api'),
-    path('calculate-depression-score/', views.calculate_depression_score, name='calculate_depression_score'),
+    
 ]
