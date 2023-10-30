104643852f1287addcd9a1c93da43f846b99e0fc
Nadeen-Badr <108485450+Nadeen-Badr@users.noreply.github.com>
2023-10-30 09:37:29 +0200

    depression_test (views/urls)update

--- apps/depression_test/urls.py
+++ apps/depression_test/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
 from . import views
-from .views import TestQuestionAPIView
+from .views import *
 
 urlpatterns = [
     path('testu/', views.TestAPIView.as_view(), name='test-api-class-view'),
     path('questions/', TestQuestionAPIView.as_view(), name='test_questions_api'),
+    
     
 ]
--- apps/depression_test/views.py
+++ apps/depression_test/views.py
@@ -1,14 +1,32 @@
-from django.http import JsonResponse
-from rest_framework.response import Response
-from rest_framework.views import APIView
-from .models import TestQuestion
+# from django.http import JsonResponse
+# from rest_framework.response import Response
+# from rest_framework.views import APIView
+# from .models import TestQuestion
 
-# Using APIView class
-class TestAPIView(APIView):
-    def get(self, request, *args, **kwargs):
-        return Response({"message": "Hello from ds!"})
+# # Using APIView class
+# class TestAPIView(APIView):
+#     def get(self, request, *args, **kwargs):
+#         return Response({"message": "Hello from ds!"})
     
     
+# class TestQuestionAPIView(APIView):
+#     def get(self, request):
+#         questions = TestQuestion.objects.all()
+#         data = []
+#         for question in questions:
+#             options = question.answer_options.all().values('value', 'label')
+#             data.append({
+#                 'question': question.question,
+#                 'answer_options': list(options)
+#             })
+#         return Response(data)
+    
+    
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .models import TestQuestion, AnswerOption
+
 class TestQuestionAPIView(APIView):
     def get(self, request):
         questions = TestQuestion.objects.all()
@@ -20,5 +38,36 @@ class TestQuestionAPIView(APIView):
                 'answer_options': list(options)
             })
         return Response(data)
-    
+
+    def post(self, request):
+        answers = request.data.get('answers', [])
+        total_score = 0
+
+        for answer in answers:
+            answer_value = answer.get('value')
+            try:
+                # Find the answer option with the matching value
+                option = AnswerOption.objects.get(value=answer_value)
+                total_score += option.value
+            except AnswerOption.DoesNotExist:
+                return Response({'error': 'Invalid answer value'}, status=status.HTTP_400_BAD_REQUEST)
+
+        # Determine the level of depression based on the total_score
+        level_of_depression = 'no depression'
+        if 6 <= total_score <= 10:
+            level_of_depression = 'normal but unhappy'
+        elif 11 <= total_score <= 25:
+            level_of_depression = 'mild depression'
+        elif 26 <= total_score <= 50:
+            level_of_depression = 'moderate depression'
+        elif 51 <= total_score <= 75:
+            level_of_depression = 'severe depression'
+        elif 76 <= total_score <= 100:
+            level_of_depression = 'extreme depression'
+
+        return Response({'total_score': total_score, 'level_of_depression': level_of_depression})
+
+class TestAPIView(APIView):
+    def get(self, request, *args, **kwargs):
+        return Response({"message": "Hello from ds!"})
     
