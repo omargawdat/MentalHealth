5b0db8b094544e1a598c9dfd7d6b973a41388a84
omargawdat <omargawdaat@gmail.com>
2024-03-11 13:10:01 +0200

    fix(logs): write logs in the file

--- MentalHealth/settings/prod.py
+++ MentalHealth/settings/prod.py
@@ -9,6 +9,12 @@ DATABASES = {
 }
 
 ALLOWED_HOSTS = ['*']
+import os
+
+LOGGING_DIR = '/var/log/MentalHealth'  # Adjust 'your_app' to the name of your application
+if not os.path.exists(LOGGING_DIR):
+    os.makedirs(LOGGING_DIR, exist_ok=True)
+
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
@@ -19,6 +25,14 @@ LOGGING = {
         },
     },
     'handlers': {
+        'file': {
+            'level': 'ERROR',
+            'class': 'logging.handlers.RotatingFileHandler',
+            'filename': os.path.join(LOGGING_DIR, 'django_error.log'),  # Log file path
+            'formatter': 'verbose',
+            'maxBytes': 1024 * 1024 * 5,  # 5 MB
+            'backupCount': 5,  # Keep 5 backup logs
+        },
         'console': {
             'level': 'ERROR',
             'class': 'logging.StreamHandler',
@@ -27,7 +41,7 @@ LOGGING = {
     },
     'loggers': {
         'django': {
-            'handlers': ['console'],
+            'handlers': ['file', 'console'],  # Here we add 'file' to the handlers list
             'level': 'ERROR',
             'propagate': True,
         },
