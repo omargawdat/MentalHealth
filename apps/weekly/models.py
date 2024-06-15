8ca07ffb2cf0b33ca8af27fb043da65bfadb0ade
omargawdat <omargawdaat@gmail.com>
2024-06-15 14:42:53 +0300

    feat: add activity life tracking models

--- apps/weekly/models.py
+++ apps/weekly/models.py
@@ -19,3 +19,20 @@ class LifeAspect(models.Model):
 
     def __str__(self):
         return f"{self.aspect_type.name}: {self.value}"
+
+
+class LifeActivity(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True, null=True)
+    icon = models.ImageField(blank=True, null=True)
+    aspect_type = models.ForeignKey(LifeAspectType, on_delete=models.CASCADE, related_name='activities')
+
+    def __str__(self):
+        return self.name
+
+
+class LifeActivityTrack(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='activity_tracks')
+    life_activity = models.ForeignKey(LifeActivity, on_delete=models.CASCADE, related_name='activity_tracks')
+    created_at = models.DateTimeField(auto_now_add=True)
+    is_checked = models.BooleanField(default=False)
